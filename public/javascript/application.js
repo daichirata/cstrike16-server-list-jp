2761ceaf90d13e66e26f1bbf2f036485bdba1b29
Dach-h <bunny.hop.md@gmail.com>
2011-11-08 10:04:53 +0900

    modify stylesheet

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,16 +1,19 @@
 (function() {
-  $(function() {
-    $("table#sortTable").tablesorter({
-      sortList: [[0, 0]]
+    $(function() {
+        $("table#sortTable").tablesorter({
+            sortList: [[0, 0]]
+        });
+        $("#reload").click(function(){
+            $(this).attr("disabled", "disabled");
+        });
+        return $("a[rel^='prettyPopin']").prettyPopin({
+            modal: true,
+            width: 600,
+            height: 260,
+            opacity: 0.5,
+            animationSpeed: '0',
+            followScroll: true,
+            loader_path: '/images/prettyPopin/loader.gif'
+        });
     });
-    return $("a[rel^='prettyPopin']").prettyPopin({
-      modal: true,
-      width: 600,
-      height: 260,
-      opacity: 0.5,
-      animationSpeed: '0',
-      followScroll: true,
-      loader_path: '/images/prettyPopin/loader.gif'
-    });
-  });
 }).call(this);
